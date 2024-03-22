936fb9f79a111db9c7574927cf489d9b9d8cd1f6
xyh <xyh9896@gmail.com>
2024-03-21 22:58:47 -0400

    add today's change

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -15,10 +17,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snipperView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Displaying a specific snippet")) //this is the respones body
+	//w.Write([]byte("Displaying a specific snippet")) //this is the respones body
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snipper with ID %v\n", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+
+		//w.WriteHeader(405)
+		//w.Write([]byte("Method not allowed"))
+		//another simplified way
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
 	w.Write([]byte("Create a new snippet...")) //this is the respones body
 }
 
