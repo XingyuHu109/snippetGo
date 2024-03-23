b905c0393cf0337d938893cb59f8f163bdcf4003
xyh <xyh9896@gmail.com>
2024-03-22 20:25:23 -0400

    snapshot before adding database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+// this is a struct that holds all the application-wide dependencies
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	//creating a command line flag
+	// to call the flag use $ go run ./cmd/web/ -addr=":80"
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	//parse the command line flag
+	flag.Parse()
+
+	//create new loggers to separate information and errors.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	//putting these 2 log dependencies into the struct
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %v\n", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module snippetbox.xyh.net
 
 go 1.22
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.0 // indirect
+)
